800d90cd78c050eb9e9f73be18d63de1530ee068
Asilbek Norboev <asilbeknorboev@gmail.com>
2025-07-07 11:46:42 -0500

    added more levels

--- src/utils/game.js
+++ src/utils/game.js
@@ -1,4 +1,4 @@
 // src/utils/game.js
 
 // thresholds stay here
-export const LEVEL_THRESHOLDS = [0, 200, 500, 1000, 2000];
+export const LEVEL_THRESHOLDS = [0, 200, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 20000];
